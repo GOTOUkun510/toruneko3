ba91df58fa4df36ada00c7008c8cbff4e96f0f57
GOTOUkun510 <gotou35@gmail.com>
2026-05-27 05:03:57 +0900

    サイドバーリンク内にスラッシュ直書き

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -17,7 +17,6 @@ export default function Sidebar() {
   const pathname = usePathname()
   const navClass = (href: string) =>
     `hover:text-white transition-colors ${pathname === href ? 'text-white font-bold' : 'text-gray-400'}`
-  const sep = <span className="text-gray-600"> / </span>
 
   return (
     <div className="fixed top-0 left-0 h-full w-[200px] bg-[#111] border-r border-[#333] flex flex-col py-4 z-50 overflow-y-auto">
@@ -31,26 +30,18 @@ export default function Sidebar() {
 
         <div className="px-3 pt-3 pb-1 text-xs text-gray-500">アイテム</div>
         <div className="px-3 text-sm leading-6">
-          <Link href="/item/weapon" className={navClass('/item/weapon')}>武器</Link>
-          {sep}
-          <Link href="/item/claw" className={navClass('/item/claw')}>爪</Link>
-          {sep}
-          <Link href="/item/shield" className={navClass('/item/shield')}>盾</Link>
-          {sep}
+          <Link href="/item/weapon" className={navClass('/item/weapon')}>武器 / </Link>
+          <Link href="/item/claw" className={navClass('/item/claw')}>爪 / </Link>
+          <Link href="/item/shield" className={navClass('/item/shield')}>盾 / </Link>
           <Link href="/item/ring" className={navClass('/item/ring')}>指輪</Link>
           <br />
-          <Link href="/item/arrow" className={navClass('/item/arrow')}>飛び道具</Link>
-          {sep}
-          <Link href="/item/food" className={navClass('/item/food')}>食料</Link>
-          {sep}
+          <Link href="/item/arrow" className={navClass('/item/arrow')}>飛び道具 / </Link>
+          <Link href="/item/food" className={navClass('/item/food')}>食料 / </Link>
           <Link href="/item/grass" className={navClass('/item/grass')}>草</Link>
           <br />
-          <Link href="/item/scroll" className={navClass('/item/scroll')}>巻物</Link>
-          {sep}
-          <Link href="/item/staff" className={navClass('/item/staff')}>杖</Link>
-          {sep}
-          <Link href="/item/pot" className={navClass('/item/pot')}>壷</Link>
-          {sep}
+          <Link href="/item/scroll" className={navClass('/item/scroll')}>巻物 / </Link>
+          <Link href="/item/staff" className={navClass('/item/staff')}>杖 / </Link>
+          <Link href="/item/pot" className={navClass('/item/pot')}>壷 / </Link>
           <Link href="/item/other" className={navClass('/item/other')}>その他</Link>
         </div>
 
