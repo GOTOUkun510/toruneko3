dc4785f527d7c6a0ebe254e0566f931a56c4e887
GOTOUkun510 <gotou35@gmail.com>
2026-05-27 07:29:30 +0900

    nowrap monster name cell

--- src/components/MonsterTable.tsx
+++ src/components/MonsterTable.tsx
@@ -67,7 +67,7 @@ export default function MonsterTable({ monsters }: { monsters: Monster[] }) {
       <tbody>
         {monsters.map((m, i) => (
           <tr key={`${m.name}-${i}`}>
-            <td>
+            <td style={{ whiteSpace: 'nowrap' }}>
               <MonsterName name={m.name} />
             </td>
             <td>{m.floors}</td>
